6f69d5e2757c0f4132f99321ddfa27f5287f8746
Bruno Goulart <bruno-goulart1@live.com>
2022-01-27 22:57:55 -0300

    Criado rota de consulta de filmes

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage ="O campo título é obrigatorio")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatorio")]
