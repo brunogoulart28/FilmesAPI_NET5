76eb55b177a813ffd5807638ef350ae002dc8f85
Bruno Goulart <bruno-goulart1@live.com>
2022-01-27 22:04:23 -0300

    Criado Validação dos campos

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,17 +1,21 @@
 ﻿using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace FilmesAPI.Controllers
 {
     [ApiController]
-    [Route("controller")]
+    [Route("filme")]
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
         public void AdicionarFilme(Filme filme)
         {
             filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
         }
     }
 }
