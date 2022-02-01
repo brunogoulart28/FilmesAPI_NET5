1e8ddd83ddc562374a2f678f0be58d9034a797ac
Bruno Goulart <bruno-goulart1@live.com>
2022-01-31 22:24:40 -0300

    Criado atualização de registro e deleção, criado o automapper

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo título é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
